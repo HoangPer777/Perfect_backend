641e99bb47b7becf07627a1550c6125cbba0891c
HoangPer777 <phanhoang03505@gmail.com>
2026-06-27 23:10:16 +0700

    feat: add price product

--- src/main/java/com/perfectmarket/modules/product/dto/request/CreateProductRequest.java
+++ src/main/java/com/perfectmarket/modules/product/dto/request/CreateProductRequest.java
@@ -1,7 +1,8 @@
 package com.perfectmarket.modules.product.dto.request;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.UUID;
 
-public record CreateProductRequest(UUID id, String title, String description,
+public record CreateProductRequest(UUID id, String title, String description, BigDecimal price,
                                    String thumbnailUrl, String status, List<String> images, List<UUID> categories) { }
