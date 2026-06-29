ae2c63fce97b2ec0f988a9e7c718546453034de9
Tran-3110 <22130276@st.hcmuaf.edu.vn>
2026-06-29 13:09:24 +0700

    feat(admin): add get user list admin api

--- src/main/java/com/perfectmarket/modules/auth/UserRepository.java
+++ src/main/java/com/perfectmarket/modules/auth/UserRepository.java
@@ -1,6 +1,12 @@
 package com.perfectmarket.modules.auth;
 
+import com.perfectmarket.modules.admin.dto.response.SnapshotUserResponse;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
 import java.util.Optional;
 import java.util.UUID;
 
@@ -9,4 +15,10 @@ public interface UserRepository extends JpaRepository<User, UUID> {
     Optional<User> findByUsername(String username);
     Boolean existsByEmail(String email);
     Boolean existsByUsername(String username);
+
+    Optional<User> findById(UUID id);
+
+    @EntityGraph(attributePaths = {"roles"})
+    @Query("SELECT u FROM User u")
+    Page<SnapshotUserResponse> findUserList(Pageable pageable);
 }
